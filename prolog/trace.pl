d2b64caf4e8311749505e682f351b8dd20adc908
sebastien.mosser <sebastien.mosser@e651b30c-f128-6be0-6592-3f1e18d9e0c3>
2010-09-23 19:40:08 +0000

    trace model, improved

--- prolog/trace.pl
+++ prolog/trace.pl
@@ -20,15 +20,15 @@
 %% @author   Main    Sebastien Mosser  [ sm@gcoke.org ]
 %%%%
 :- module(trace,[]).
-
+:- channels:declare(trace(shell)).
 
 %%%%
-%% Pebble-based system.
+%% Model instantiation
 %%%%
 
 %% build_empty/1: build_empty(-T):
 % unify T with an empty trace, unique in the system (We hope, but it should).
-buid_empty(gcoke_trace(Id,[])) :- 
+build_empty(gcoke_trace(Id,[])) :- 
 	gensym(gtrace_,Functor), get_time(Timestamp), 
 	Id =.. [Functor, Timestamp].
 
@@ -36,14 +36,96 @@ buid_empty(gcoke_trace(Id,[])) :-
 % Unify Id with the (assumed) unique identifier associated to T.
 read_identifier(gcoke_trace(Id,_),Id).
 
-%% push_pebble/5: push_pebble(+Trace, +Origin, +Destination, +Message, -Trace')
+%%%%
+%% Pushing data into a Trace model.
+%%%%
+
+%% push/5: push(+Trace, +Origin, +Destination, +Message, -Trace')
 % Unify Trace' with Trace, enhanced by a new peeble build from Origin to
-% Destination, associated to Message.
-push_peeble(Trace, Origin, Destination, Message, Trace_prime) :- 
+% Destination, associated to Message. (1 -> 1 trace)
+push(Trace, Origin, Destination, Message, Trace_prime) :- 
 	Trace = gcoke_trace(Id,L),
-	Trace_prime = gtrace(Id,[pebble(Origin, Destination, Message)|L]).
-	
+	Trace_prime = gcoke_trace(Id,[pebble(Origin, Destination, Message)|L]).
 
+%% multi_push/5: multi_push(+Trace, +Origins, +Destination, +Message, -Trace')
+% Same as 'push', but works on a list of Origins (n -> 1 trace).
+multi_push(Trace, [], _, _, Trace) :- !.
+multi_push(Trace, [H|T], Destination, Message, Trace_prime) :- 
+	push(Trace, H, Destination, Message, Tmp),
+	multi_push(Tmp, T, Destination, Message, Trace_prime).
 
-explore(Trace, Origin, Destination, Message) :- 
-	member(pebble(Origin, Destination, Message), Trace).
+%%%%
+%% Trace exploration
+%%%%
+
+%% has_for_ancestor/3: has_for_ancestor(+Trace, +Element, -Ancestor)
+% Extract an Ancestor of an Element in Trace.
+has_for_ancestor(Trace, Element, Ancestor) :- 
+	Trace = gcoke_trace(_, Pebbles),
+	member(pebble(Ancestor, Element, _), Pebbles).
+has_for_ancestor(Trace, Element, Ancestor) :- 
+	Trace = gcoke_trace(_, Pebbles),
+	member(pebble(X, Element, _), Pebbles), 
+	has_for_ancestor(Trace, X, Ancestor).
+
+%% has_for_descendant/3: has_for_descendant(+Trace, +Element, -Descendant)
+% Extract a Descendant of an Element in Trace.
+has_for_descendant(Trace, Element, Descendant) :-
+	Trace = gcoke_trace(_, Pebbles),
+	member(pebble(Element, Descendant, _), Pebbles).
+has_for_descendant(Trace, Element, Descendant) :-
+	Trace = gcoke_trace(_,Pebbles), member(pebble(Element, X, _), Pebbles), 
+	has_for_descendant(Trace, X, Descendant).
+
+%%%%
+%% Representation of Trace model (textual & graphical)
+%%%%
+
+%% as_text/2: as_text(+Trace, -Text):
+% Unify Text with a textual representation of Trace.
+as_text(gcoke_trace(_,L), Text) :- 
+	findall(S,( member(pebble(X,Y,Z),L), 
+	            swritef(S,'%w -> %w (%s)',[X,Y,Z])), Raw),
+	sort(Raw, To_print), swrite_list(To_print, ',\n', '  - ', Text).
+
+%% as_dot/2: as_dot(+Trace, -Dot_code):
+% Unify Dot_code with a Graphviz representation of Trace
+as_dot(gcoke_trace(_,L), Dot_code) :- 
+	findall(S,( member(pebble(X,Y,Z),L), 
+	            swritef(S,'%w -> %w [label="%s"];',[X,Y,Z])), Raw_edges),
+	swrite_list(Raw_edges, '\n', '  ', Edges),
+	swrite_list(['fontname = Courier;', 'node [fontname = "Courrier"];',
+		     'edge [fontname = "Courrier"];', 'rankdir=LR;'],
+		    '\n','  ',Head),
+	swritef(Dot_code,'digraph trace_model {\n%w\n%w\n}',[Head, Edges]).
+
+%% as_picture/3: as_picture(+Trace, +Format, +File_name)
+% Create a file File_name.Format, containing a graph. representation of Trace.
+as_picture(Trace, Format, F) :- 
+	as_dot(Trace, Dot_code), tmp_file('trace_to_dot',Tmp), 
+	open(Tmp, write, Stream), write(Stream, Dot_code), close(Stream),
+        getenv_or_default('GCOKE_DOT','dot',E),
+	expand_file_name(F,[File]),
+        swritef(Cmd,'%w -T%w %w > %w.%w', [E, Format, Tmp, File, Format]), 
+        channels:push(trace(shell),Cmd,[]),
+        shell(Cmd).
+
+%% show/1: show(+Trace)
+% Open a graphical visualizer which depicts the content of Trace.
+show(Trace) :- 
+	tmp_file('trace_to_png',Tmp), as_picture(Trace, png, Tmp), 
+	getenv_or_default('GCOKE_OPEN','open',E),
+        swritef(Cmd,'%w %w.png', [E, Tmp]), channels:push(trace(shell),Cmd,[]),
+        shell(Cmd).
+
+%%%%
+%% Test
+%%%%
+
+%% test(Trace) :- 
+%% 	build_empty(T1), 
+%% 	push(T1, a, b, "a to b", T2), push(T2, b, c, "b to c", T3),
+%% 	multi_push(T3, [d,e], h, "(d,e) to h", T4), 
+%% 	multi_push(T4, [f,g], i, "(f,g) to i", T5), 
+%% 	multi_push(T5, [h,i], j, "(h,i) to j", T6), 
+%% 	push(T6, j, k, "j to k", Trace).
